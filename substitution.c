c9165d71eadb62aa947728e950bc51347de0fde8
Matheus Tito <mthsilva0@gmail.com>
2025-01-27 14:43:34 -0300

    Added a validator function to keep the code clean

--- substitution.c
+++ substitution.c
@@ -1,52 +1,61 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-int substituteCharacters (char key[26], char text[30]);
+int substituteCharacters(char key[26], char text[30]);
+bool isTextAlphabetical(char text);
 
-int main (void) {
-    char keyInput[26], key;
-    char plainText[30];
+int main(void) {
+    char keyInput[26];
+    char plainText[100];
 
     do {
         printf("Key: ");
-        scanf("%s", keyInput);
+        scanf("%26s", keyInput);
     } while (strlen(keyInput) != 26);
 
-    key = toupper(keyInput);
+    for (int i = 0; i < 26; i++) {
+        keyInput[i] = toupper(keyInput[i]);
+    }
 
     printf("Plain text: ");
-    scanf( " %99[^\n]", plainText);
+    scanf(" %99[^\n]", plainText);
 
-    //printf("%s",plainText);
-
-    substituteCharacters(key, plainText);
+    substituteCharacters(keyInput, plainText);
 
     return 0;
 }
 
-int substituteCharacters (char key[26], char text[30]) {
+int substituteCharacters(char key[26], char text[30]) {
     char encodedText[30], toCompare[30];
     char base = 'A';
 
     printf("Encoded text: ");
 
     for (int i = 0; i < strlen(text); i++) {
-        if (isspace(text[i])) {
-            printf(" ");
-        }
-        
-        if (isalpha(text[i])) {
+        if (isTextAlphabetical(text[i])) {
             toCompare[i] = toupper(text[i]);
 
-            //TO-DO: Regra que se a letra for A (todas da toCompare estão como upper) então ela corresponde a key[0], e assim suscetivamente.
-            //toCompare[i];
-        } else {
-            encodedText[i] = text[i];
-        }
+            //TO-DO: Regra que se a letra for A então ela corresponde a key[0], e assim suscetivamente.
 
-        printf("%c", encodedText[i]);
+            printf("%c", encodedText[i]);
+        }
     }
 
     return 0;
+}
+
+bool isTextAlphabetical(char text) {
+    if (isalpha(text)) {
+        return true;   
+    }
+        
+    if (isspace(text)) {
+        printf(" ");
+        return false;
+    }
+           
+    printf("%s", text);
+    return false;
 }
